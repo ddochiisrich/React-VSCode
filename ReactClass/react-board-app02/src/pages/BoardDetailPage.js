29eabd1b490699f5dc1ac345941cc5f4cfe17186
ddochiisrich <ddochiisrich@gmail.com>
2024-06-23 15:49:46 +0900

    복습

---
+++ ReactClass/react-board-app02/src/pages/BoardDetailPage.js
@@ -0,0 +1,13 @@
+<div className="row my-5" id="global-content">
+				<div className="col-10 offset-1">
+					<form name="checkForm" id="checkForm">
+						<input type="hidden" name="no" id="no" />
+						<input type="hidden" name="pass" id="rPass" />
+					</form>
+					<div className="row text-center">
+						<div className="col">
+							<h2 className="fs-3 fw-bold">게시 글 상세보기</h2>
+						</div>
+					</div>
+        </div>
+      </div>
