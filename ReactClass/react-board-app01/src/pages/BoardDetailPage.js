2dc4b258d62ba033f2e5417039124b8c983668f2
ddochiisrich <ddochiisrich@gmail.com>
2024-06-24 19:06:51 +0900

    6/24 수업

--- ReactClass/react-board-app01/src/pages/BoardDetailPage.js
+++ ReactClass/react-board-app01/src/pages/BoardDetailPage.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {useSearchParams, Link} from 'react-router-dom';
+import {useSearchParams, Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 
@@ -18,13 +18,66 @@ export default function BoardDetailPage() {
 		getBoard();
 	},[])
 
+	const navigate = useNavigate();
+
+	const [pass, setPass] = useState("");
+	const handlePassChange = e => {
+		setPass(e.target.value);
+	}
+
+	const handleBtnClick = async(e) => {
+		// 비밀번호 읽어오기
+
+		// 수정하기 버튼 또는 삭제하기 버튼 중 어떤 버튼이 클릭되었는지 판단
+		const isUpdate = e.target.id == "detailUpdate"; 
+
+		// 비밀번호 입력이 안되었을때
+		if(pass.length == 0){
+			alert(`게시글을 ${isUpdate ? "수정" : "삭제"}하려면 비밀번호를 입력하세요`);
+			return;
+		}
+
+		// 비밀번호 입력이 되었을 때
+
+		if(isUpdate){	// 수정버튼 클릭
+		// axios를 이용해서 서버로 요청을 보내고 응답이 false 면 비밀번호가 틀리다고 안내 true 수정폼으로 이동 
+			await axios.get(`http://localhost:3010/update/${no}/${pass}`)
+			.then(res => {
+				console.log(res.data);
+				if(! res.data.result){
+					alert("비밀번호가 틀립니다.");
+					return;
+				}
+				// 비밀번호가 맞으면 수정폼으로 이동
+				navigate(`/boardUpdate?no=${no}`);
+			})
+			.catch(err => {
+				console.log("err : ", err);
+			});
+		} else { // 삭제버튼 클릭
+		// axios를 이용해서 서버로 요청을 보내고 응답이 false 면 비밀번호가 틀리다고 안내 true 게시글 리스트로 이동 
+			await axios.post(`http://localhost:3010/delete`, {no: no, pass: pass})
+			.then(res => {
+				console.log(res);
+				if(! res.data.result){
+					alert("비밀번호가 틀립니다.");
+					return;
+				}
+				navigate("/boardList", { replace: true });
+			})
+			.catch(err => console.log("err", err));
+		}
+			
+
+
+
+	}
+
   return(
     // Content
     	<div className="row my-5" id="global-content">
 				<div className="col-10 offset-1">
 					<form name="checkForm" id="checkForm">
-						<input type="hidden" name="no" id="no" />
-						<input type="hidden" name="pass" id="rPass" />
 					</form>
 					<div className="row text-center">
 						<div className="col">
@@ -50,7 +103,8 @@ export default function BoardDetailPage() {
 										<th>비밀번호</th>
 										<td>
 											<div className="col-sm-8">
-												<input className="form-control" type="password" name="pass" id="pass" />
+												<input className="form-control" type="password" name="pass" id="pass" 
+												onChange={ handlePassChange } value={pass}/>
 											</div>
 										</td>
 										<th>조회수</th>
@@ -73,8 +127,8 @@ export default function BoardDetailPage() {
 					</div>
 					<div className="row my-3">
 						<div className="col text-center">
-							<input className="btn btn-warning" type="button" id="detailUpdate" value="수정하기"/>
-							&nbsp;&nbsp;<input className="btn btn-danger"  type="button" id="detailDelete" value="삭제하기" />			
+							<input className="btn btn-warning" type="button" id="detailUpdate" value="수정하기" onClick={ handleBtnClick }/>
+							&nbsp;&nbsp;<input className="btn btn-danger"  type="button" id="detailDelete" value="삭제하기" onClick={ handleBtnClick }/>			
 							&nbsp;&nbsp;<Link to="/boardList" className="btn btn-primary">목록보기</Link>				
 						</div>
 					</div>
--- ReactClass/react-board-app01/src/pages/BoardUpdateFormPage.js
+++ ReactClass/react-board-app01/src/pages/BoardUpdateFormPage.js
@@ -1,14 +1,224 @@
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { useState, useCallback, useEffect } from "react";
+import axios from "axios";
+
 export default function BoardUpdateFormPage() {
-  return(
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const no = searchParams.get("no");
+
+  const [board, setBoard] = useState({
+    no: "",
+    writer: "",
+    title: "",
+    pass: "",
+    content: "",
+  });
+
+  // no에 해당하는 게시글을 읽어와 폼에 출력
+  const getBoard = async() => {
+    const res = await axios.get(`http://localhost:3010/boardDetail?no=${no}`);
+    res.data.pass="";
+    setBoard(res.data);
+  }
+
+  useEffect(() => {
+    getBoard();
+  }, []);
+
+  const navigate = useNavigate();
+
+  const [inputed, setInputed] = useState({
+    writer: false,
+    title: false,
+    pass: false,
+    content: false,
+  });
+
+  const handleBlur = (e) => {
+    setInputed({ ...inputed, [e.target.name]: true });
+  };
+
+  const validate = useCallback((e) => {
+    const errors = {
+      writer: "",
+      pass: "",
+      title: "",
+      content: "",
+    };
+
+    if (!board.writer) {
+      errors.writer = "작성자를 입력하세요";
+    }
+    if (!board.title) {
+      errors.title = "제목을 입력하세요";
+    }
+    if (!board.pass) {
+      errors.pass = "비밀번호를 입력하세요";
+    }
+    if (!board.content) {
+      errors.content = "내용을 입력하세요";
+    }
+    return errors;
+  }, [board]);
+
+	useEffect(() => {
+		const errors = validate();
+		setErrors(errors);
+	}, [validate])
+
+  const handleChange = (e) => {
+    setBoard({
+      ...board,
+      [e.target.name]: e.target.value,
+    });
+    console.log("change : ", { ...board, [e.target.name]: [e.target.value] });
+  };
+
+  const [errors, setErrors] = useState({
+    writer: "",
+    pass: "",
+    title: "",
+    content: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+		setInputed({
+			writer: true,
+			pass:true,
+			title: true,
+			content: true
+		})
+
+    // 유효성 검사 - alert();
+    // validate 이용
+    const errors = validate();
+    setErrors(errors);
+
+    if (Object.values(errors).some((v) => v)) {
+      console.log(errors);
+      return;
+    }
+
+    // 유효성 검사가 정상적으로 모두 통과되면 axios를 이용해서
+    // 백엔드 서버에 데이터를 전송 - 통신이 정상적으로 완료되면
+    // navigate를 이용해서 게시 글 리스트로 라우팅 되도록 구현
+    await axios
+      .post("http://localhost:3010/update", board)
+      .then((res) => {
+        console.log(res);
+        // 비번이 틀린경우
+        if(! res.data.result){
+          alert("비밀번호가 틀림");
+          return
+        }
+        // 비번이 맞는경우
+        navigate("/boardList", { replace: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log("폼 데이터 전송");
+  };
+
+  return (
     // Content
-    			<div class="row my-5" id="global-content">
-				<div class="col-10 offset-1">
-					<div class="row text-center">
-						<div class="col">
-							<h2 class="fs-3 fw-bold">게시 글 수정</h2>
-						</div>
-					</div> 
+    <div className="row my-5" id="global-content">
+      <div className="col-10 offset-1">
+        <div className="row text-center">
+          <div className="col">
+            <h2 className="fs-3 fw-bold">게시 글 수정하기</h2>
+          </div>
         </div>
+        <form
+          name="writeForm"
+          action="writeBoard"
+          id="writeForm"
+          className="row g-3 border-primary"
+          method="post"
+          onSubmit={handleSubmit}
+					
+        >
+          <div className="col-4 offset-md-2">
+            <label htmlFor="writer" className="form-label">
+              글쓴이
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="writer"
+              id="writer"
+              placeholder="작성자를 입력해 주세요"
+              value={board.writer}
+              onChange={handleChange}
+							onBlur={ handleBlur }
+            />
+            {inputed.writer && errors.writer && (
+              <p className="text-danger p-1 m-0">{errors.writer}</p>
+            )}
+          </div>
+          <div className="col-4 ">
+            <label htmlFor="pass" className="form-label">
+              비밀번호
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              name="pass"
+              id="pass"
+              value={board.pass}
+              onChange={handleChange}
+							onBlur={ handleBlur }
+            />
+            {inputed.pass && errors.pass && (
+              <p className="text-danger p-1 m-0">{errors.pass}</p>
+            )}
+          </div>
+          <div className="col-8 offset-md-2">
+            <label htmlFor="title" className="form-label">
+              제 목
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="title"
+              id="title"
+              value={board.title}
+              onChange={handleChange}
+							onBlur={ handleBlur }
+            />
+            {inputed.title && errors.title && (
+              <p className="text-danger p-1 m-0">{errors.title}</p>
+            )}
+          </div>
+          <div className="col-8 offset-md-2">
+            <label htmlFor="content" className="form-label">
+              내 용
+            </label>
+            <textarea
+              className="form-control"
+              name="content"
+              id="content"
+              rows="10"
+              value={board.content}
+              onChange={handleChange}
+							onBlur={ handleBlur }
+            />
+            {inputed.content && errors.content && (
+              <p className="text-danger p-1 m-0">{errors.content}</p>
+            )}
+          </div>
+          <div className="col-8 offset-md-2 text-center mt-5">
+            <input type="submit" value="수정하기" className="btn btn-primary" />
+            &nbsp;&nbsp;
+            <Link to="/boardList" className="btn btn-primary">
+              목록보기
+            </Link>
+          </div>
+        </form>
       </div>
+    </div>
   );
 }
